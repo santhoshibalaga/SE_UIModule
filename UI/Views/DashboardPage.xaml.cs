187b97d28bc9310e0815bf7042a9f73f1856cf73
santhoshibalaga <112101005@smail.iitpkd.ac.in>
2024-10-24 14:49:01 +0530

    UI code pushed

---
+++ UI/Views/DashboardPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using UI.ViewModels;
+
+namespace UI.Views
+{
+    public partial class DashboardPage : Page
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+
+            this.DataContext = new DashboardViewModel();
+        }
+
+        
+    }
+}
